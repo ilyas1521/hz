6d52bcab32def82f2314e62d66c97877da113f1d
ilyam <ilya.metelkin.2005@mail.ru>
2022-05-05 23:42:40 +0300

    v.2

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ from collections import Counter
 from itertools import product
 
 a =[]
-
-combs = list(product('12', repeat=4))
+combs = list(product('12', repeat=3))
 for comb in combs:
-    x = 81
+    x = 2
     comb = ''.join((comb))
     for i in range(len(comb)):
         if comb[i] == '1':
-            x = int(x/3)
+            x = x+2
 
         if comb[i] == '2':
-            x = x*5
+            x = x*3
 
     a.append(x)
 
+a.sort()
+print(len(a))
 print(Counter(a))
 
